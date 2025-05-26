93740e7d5d3b4ba0040b932fc4734327af29eb20
abbas <abbas1795khan@gmail.com>
2025-05-26 11:29:22 +0500

    ..

--- MyPython.py
+++ MyPython.py
@@ -1,10 +1,12 @@
-
+import sys
 from datetime import datetime
+
 def calculate_age(dob):
     today = datetime.today()
     dob = datetime.strptime(dob, '%Y-%m-%d')
     age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
     return age
 
-
-#end
+if __name__ == "__main__":
+    dob = sys.argv[1]
+    print(f"Age: {calculate_age(dob)}")
